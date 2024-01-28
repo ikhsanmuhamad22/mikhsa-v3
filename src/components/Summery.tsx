17ebef85153e3819e16bfd0c10441a9f1332cfdf
ikhsanmuhamad22 <ikhsanmuhamad3885@gmail.com>
2024-01-28 10:35:06 +0700

    add: summery page

--- src/components/Summery.tsx
+++ src/components/Summery.tsx
@@ -2,12 +2,25 @@ import avatar from '../assets/easlo-laptop.svg';
 
 const Summery = () => {
   return (
-    <section className="pt-10 h-screen">
-      <h1 className="font-extrabold text-3xl">Portfolio</h1>
-      <div className="flex">
-        <div id="text-summery"></div>
-        <div id="avatar">
-          <img src={avatar} alt="" />
+    <section className="pt-10 h-full">
+      <h1 className="font-bold text-4xl">Portfolio</h1>
+      <div className="flex my-10">
+        <div id="text-summery" className="flex-1">
+          <h2 className="text-2xl font-bold mb-3">Hello i'm Ihsan Muhammad</h2>
+          <p className="text-base font-medium ">
+            Vocational school graduates who are interested in programming
+            technology and software, have experience to create and develop
+            several frontend and backend web projects such as CSM articles and
+            online stores.
+          </p>
+          <br />
+          <p className="text-base font-medium">
+            as well as internship experience as a frontend developer, always
+            eager and very active in learning new technologies.
+          </p>
+        </div>
+        <div id="img-summery" className="flex flex-1 justify-center">
+          <img className="h-80" src={avatar} alt="" />
         </div>
       </div>
     </section>
